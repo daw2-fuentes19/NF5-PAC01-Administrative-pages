f0c03ca3e89f140d7ed4fe98ea1f4a054cce62c3
daw2-fuentes19 <43651378+daw2-fuentes19@users.noreply.github.com>
2019-12-02 17:58:50 +0100

    Add files via upload

---
+++ N5P101comments.php
@@ -0,0 +1,21 @@
+<?php
+
+echo "List of the 10 most important concepts";
+echo "<br/>";
+echo "<li>UPDATE TABLES</li>";
+echo "<li>DELETE TABLES</li>";
+echo "<li>FUNCTIONS</li>";
+echo "<li>mysqli_select</li>";
+echo "<li>ID POST</li>";
+echo "<li>VARIABLES GET</li>";
+echo "<li>VARIABLES POST</li>";
+echo "<li>INPUTS</li>";
+echo "<li>INSERT INPUTS</li>";
+echo "<li>INPUTS GET</li>";
+echo "<br/>";
+echo "NOTA DEL DOCUMENTO: 7";
+echo "<br/>";
+echo "NOTA ESTUDIANTE: 9";
+echo "<br/>";
+echo "EXPLICA UNA MEJORA: No veo nada a cambiar.";
+?>
